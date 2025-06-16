6231429119e4ae40bf7a590284a3dcabfd18b82e
paufrancisco <francisco.paulo.pf@gmail.com>
2025-06-16 20:59:57 +0800

    video 3

--- my-diary/src/App.js
+++ my-diary/src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.css';  
+import Header from './components/Header';
+import Footer from './components/Footer';
+import main from './css/main.css';
 
 function App() {
+
+  const firstName = 'Paulo';
+  const lastName ='Francisco';
+  const age = 30;
+  const students = {
+    firstName: 'Mat',
+    lastName: 'James',
+    age: 20,
+  }
+
+  const arr = [1,2,3,4,5];
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <main>
+      <Header/>
+      <h1 style={main}> 
+        {firstName} {lastName} {age} {students.firstName}
+      </h1> 
+
+      {/* map all the array */}
+      {arr.map(num => <p> {num * 5 }</p>)} 
+
+      {/* map all the object */}
+      {Object.values(students).map((val,i)=> <p key={i}>{val}</p>)}
+    </main>
+    <Footer/>
+    </>
+     
   );
 }
 
